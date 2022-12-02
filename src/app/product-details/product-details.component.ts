d4afb673ea3b680cae2c00f393050ef59efaa9f1
raymaaark <raymarkreyesl@gmail.com>
2022-12-02 13:48:37 +0800

    1 file modified

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -7,7 +7,7 @@ import { Product, products } from '../products';
   templateUrl: './product-details.component.html',
   styleUrls: ['./product-details.component.css'],
 })
-export class ProductDetailsComponent {
+export class ProductDetailsComponent implements OnInit {
   product: Product | undefined;
   constructor(private route: ActivatedRoute) {}
 
